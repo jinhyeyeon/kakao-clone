84f1e68816d83c4ca7e89516c6751733a84ea8e8
jinhyeyeon <jhy199@naver.com>
2021-03-25 17:25:37 +0900

    Header 추가

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
+import {BrowserRouter} from "react-router-dom";
 import './styles/app.scss';
+import Header from './component/Header/Header';
+import Footer from './component/Footer/Footer';
 
 function App() {
   return (
-    <div className="app">
-      test
-    </div>
+    <BrowserRouter>
+      <div className="app">
+        <Header />
+        <Footer />
+      </div>
+    </BrowserRouter>
   );
 }
 
