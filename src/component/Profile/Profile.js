a6638692070b45c79cb497fdd7c711e184a4dcdc
jinhyeyeon <jhy199@naver.com>
2021-03-31 15:25:47 +0900

    Profile 구조 변경

--- src/component/Profile/Profile.js
+++ src/component/Profile/Profile.js
@@ -7,9 +7,8 @@ const Profile = ({profileImg, onClick}) => {
     <div
       className="profile"
       onClick={onClick}
-    >
-      <img src={profileImg || 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png'} alt=""/>
-    </div>
+      style={{backgroundImage: `url(${profileImg || 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png'})`}}
+    />
   )
 }
 
