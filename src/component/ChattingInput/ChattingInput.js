04d1d5d9296be801201414567ffec5e47c1cdb74
jinhyeyeon <jhy199@naver.com>
2021-04-08 17:33:07 +0900

    ChattingInput Component 생성

---
+++ src/component/ChattingInput/ChattingInput.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './chatting-input.scss';
+
+class ChattingInput extends React.Component {
+  state = {
+  }
+
+  render() {
+    const {} = this.state;
+    return (
+      <div className="chatting-input">
+        
+      </div>
+    )
+  }
+}
+export default ChattingInput;
