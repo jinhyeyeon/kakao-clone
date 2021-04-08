c133b2814e38ce1835cc0657b7e5a867713d361c
jinhyeyeon <jhy199@naver.com>
2021-04-08 11:38:47 +0900

    profile item 스타일 수정

--- src/App.js
+++ src/App.js
@@ -5,9 +5,6 @@ import Friend from './pages/friend';
 import Chat from './pages/chat';
 import Footer from './component/Footer/Footer';
 import Header from './component/Header/Header';
-import { MENU_HOME } from './constants/menu';
-import ChattingRoom from './pages/chattingRoom';
-
 class App extends React.Component {
   state = {
 
